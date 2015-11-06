8368c73bd579f839fd0b665e41df433990c0b2f8
Danny Lewis <nitomatik@gmail.com>
2015-11-05 17:22:15 -0700

    Changing folder structure.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CPSC501-ReflectiveSerializer
 An object serializer / deserializer that operates on arbitrary objects using reflection.
+
+It is made up of 2 programs, the sender (which serializes data) and the receiver (which deserializes data). They are meant to be run on 2 separate computers so as to show that the data is sent accross a network.
---
+++ ReflectiveSerializer/src/sender/ObjectCreator.java
@@ -0,0 +1,16 @@
+package sender;
+
+public class ObjectCreator {
+
+	// TODO: simple object with only primitives for instance variables
+	
+	// TODO: object that contains references to other objects
+	
+	// TODO: object that contains an array of primitives
+	 
+	// TODO: object that contains an array of object references
+	
+	// TODO: object that uses an instance of one of Java's collection classes
+	//		 to refer to other objects (ie. List)
+	
+}
